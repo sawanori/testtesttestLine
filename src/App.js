3f158fb40b3ef5dd8b4c8ac081137631a87f3432
noritakasawada <snp.inc.info@gmail.com>
2023-04-27 14:40:28 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    liff
+      .init({
+        liffId: process.env.REACT_APP_LIFF_ID,
+      })
+      .then(() => {
+        setMessage("おいでませー");
+      })
+      .catch((e) => {
+        setMessage("LIFF init failed.");
+        setError(`${e}`);
+      });
+  });
+
   return (
     <div className="App">
-      <h1>test</h1>
-   </div>
+      <h1>ただ側にいる<br/>それが寄り添い、、、</h1>
+      {message && <p>{message}</p>}
+      {error && (
+        <p>
+          <code>{error}</code>
+        </p>
+      )}
+      <a
+        href="https://developers.line.biz/ja/docs/liff/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        寄り添い羊サービス（presented by パンツG）
+      </a>
+    </div>
   );
 }
 
